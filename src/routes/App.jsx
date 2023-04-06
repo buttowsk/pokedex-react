582a2644c753b49a9a8cc104d78b3e79c3f11d31
buttowsk <vinicius_nunes0104@hotmail.com>
2023-04-06 08:16:40 -0300

    pokeList gerado no app

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,18 +5,34 @@ import { Moves } from '../pages/Moves';
 import { Abilities } from '../pages/Abilities';
 import { Items } from '../pages/Items';
 import { Locations } from '../pages/Locations';
+import { PokePage } from '../pages/PokePage';
+import { useAllPokemons } from '../hooks/useAllPokemons/index.js';
+import { LoadingComponent } from '../components/LoadingComponent/index.jsx';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const {isLoading, pokeList} = useAllPokemons();
+  const [isPokeListLoaded, setIsPokeListLoaded] = useState(false);
 
-    return (
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/pokedex" element={<Pokedex />} />
-                <Route path="/moves" element={<Moves />} />
-                <Route path="/abilities" element={<Abilities />} />
-                <Route path="/items" element={<Items />} />
-                <Route path="/locations" element={<Locations />} />
-            </Routes>
-    )
+  useEffect(() => {
+    if (!isLoading && pokeList) {
+      setIsPokeListLoaded(true);
+    }
+  }, [isLoading, pokeList]);
+
+  if (isLoading || !isPokeListLoaded) {
+    return <LoadingComponent/>
+  }
+  return (
+    <Routes>
+      <Route path="/" element={<Home pokeList={pokeList}/>} />
+      <Route path="/pokedex" element={<Pokedex pokeList={pokeList} />} />
+      <Route path="/moves" element={<Moves />} />
+      <Route path="/abilities" element={<Abilities />} />
+      <Route path="/items" element={<Items />} />
+      <Route path="/locations" element={<Locations />} />
+      <Route path="/pokedex/:pokemon" element={<PokePage pokeList={pokeList} />} />
+    </Routes>
+  )
 }
 export default App
