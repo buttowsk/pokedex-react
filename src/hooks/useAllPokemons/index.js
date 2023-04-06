05b942e9cc11f77b11ef7976d44403f72e4f5e0d
buttowsk <vinicius_nunes0104@hotmail.com>
2023-04-06 08:24:52 -0300

    nd

--- src/hooks/useAllPokemons/index.js
+++ src/hooks/useAllPokemons/index.js
@@ -12,7 +12,7 @@ export const useAllPokemons = () => {
         const fetchPokemons = async () => {
             try {
                 setLoading(true);
-                const response = await pokeApi.get('/pokemon?limit=500&offset=0');
+                const response = await pokeApi.get('/pokemon?limit=200&offset=0');
                 const { data } = response;
                 const { results } = data;
                 const pokemonPromises = results.map(async (result) => {
