100bada1a06bb3d12c6c5f063d2de33dad8b2ef6
buttowsk <vinicius_nunes0104@hotmail.com>
2023-04-07 19:48:10 -0300

    ben loader, removendo rotas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,37 +1,32 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home';
 import { Pokedex } from '../pages/Pokedex';
-import { Moves } from '../pages/Moves';
-import { Abilities } from '../pages/Abilities';
 import { Items } from '../pages/Items';
-import { Locations } from '../pages/Locations';
 import { PokePage } from '../pages/PokePage';
 import { useAllPokemons } from '../hooks/useAllPokemons/index.js';
 import { LoadingComponent } from '../components/LoadingComponent/index.jsx';
 import { useEffect, useState } from 'react';
+import { useAllItems } from '../hooks/useAllItems/index.js';
+import { BenLoading } from '../components/BenLoading/index.jsx';
+
 
 function App() {
-  const {isLoading, pokeList} = useAllPokemons();
-  const [isPokeListLoaded, setIsPokeListLoaded] = useState(false);
+  const { items, hasMoreItems, setItemsPage, itemsLoading } = useAllItems();
+  const { isLoading, pokeList, hasMorePoke, setPokePage } = useAllPokemons();
 
-  useEffect(() => {
-    if (!isLoading && pokeList) {
-      setIsPokeListLoaded(true);
-    }
-  }, [isLoading, pokeList]);
 
-  if (isLoading || !isPokeListLoaded || !pokeList || pokeList.length === 0) {
-    return <LoadingComponent/>
+  if (isLoading || !pokeList || pokeList.length === 0 || !items) {
+    return <BenLoading/>
   }
+
+
+
   return (
     <Routes>
       <Route path="/" element={<Home pokeList={pokeList}/>} />
-      <Route path="/pokedex" element={<Pokedex pokeList={pokeList} />} />
-      <Route path="/moves" element={<Moves />} />
-      <Route path="/abilities" element={<Abilities />} />
-      <Route path="/items" element={<Items />} />
-      <Route path="/locations" element={<Locations />} />
-      <Route path="/pokedex/:pokemon" element={<PokePage pokeList={pokeList} />} />
+      <Route path="/pokedex" element={<Pokedex pokeList={pokeList} hasMore={hasMorePoke} setPage={setPokePage} loading={isLoading} />} />
+      <Route path="/items" element={<Items items={items} hasMore={hasMoreItems} setPage={setItemsPage} loading={itemsLoading} />} />
+      <Route path="/pokedex/:pokemon" element={<PokePage pokeList={pokeList}/>} />
     </Routes>
   )
 }
