8c8fe6e1b9dc20d329abfcbfc32e458208f3d130
buttowsk <vinicius_nunes0104@hotmail.com>
2023-04-07 19:48:36 -0300

    scroll

---
+++ src/hooks/useAllItems/index.js
@@ -0,0 +1,49 @@
+import { pokeApi } from '../../services/pokeApi.js';
+import { useState, useCallback, useEffect } from 'react';
+import { Item } from '../../models/Item/index.js';
+export const useAllItems = () => {
+	const [items, setItems] = useState([]);
+	const [itemsLoading, setItemsLoading] = useState(false);
+	const [error, setError] = useState(null);
+	const [itemsPage, setItemsPage] = useState(1);
+	const [totalItemsPages, setTotalItemsPages] = useState(1);
+	const [hasMoreItems, setHasMoreItems] = useState(true);
+
+	const getAllItems = useCallback(async (page) => {
+		setItemsLoading(true);
+		try {
+			const { data } = await pokeApi.get(`/item?limit=20&offset=${(page - 1) * 20}`);
+			const { results } = data;
+			const itemsPromises = results.map(async (result) => {
+				const id = result.url.split('/').slice(-2, -1)[0];
+				const { data: itemData } = await pokeApi.get(`/item/${id}`);
+				const item = new Item(
+					itemData.name,
+					itemData.sprites.default,
+					itemData.id,
+					itemData.effect_entries[0].effect,
+					itemData.cost,
+					itemData.pokemon,
+					itemData.name,
+					itemData.category.name,
+				);
+				return [id, item]; // retorna um array com o ID e o item
+			});
+			const items = await Promise.all(itemsPromises);
+			const newItems = Object.fromEntries(items); // transforma o array de items em um objeto
+			setItems((prevItems) => ({ ...prevItems, ...newItems })); // adiciona os novos items ao objeto existente
+			setTotalItemsPages(Math.ceil(data.count / 20));
+			setHasMoreItems(page < totalItemsPages);
+		} catch (err) {
+			setError(err);
+		} finally {
+			setItemsLoading(false);
+		}
+	}, [totalItemsPages]);
+
+	useEffect(() => {
+		getAllItems(itemsPage);
+	}, [getAllItems, itemsPage]);
+
+	return { items, itemsLoading, error, hasMoreItems, setItemsPage };
+};
--- src/hooks/useAllPokemons/index.js
+++ src/hooks/useAllPokemons/index.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useCallback, useEffect, useState } from 'react';
 import { pokeApi } from '../../services/pokeApi.js';
 import { Pokemon } from '../../models/Pokemon/index.js';
 import { Evolution } from '../../models/Evolution/index.js';
@@ -7,96 +7,86 @@ export const useAllPokemons = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [pokeList, setPokeList] = useState([]);
+    const [pokePage, setPokePage] = useState(1);
+    const [totalPokePages, setTotalPokePages] = useState(1);
+    const [hasMorePoke, setHasMorePoke] = useState(true);
+
+
+    const fetchPokemons = useCallback(async (page) => {
+        try {
+            setLoading(true);
+            const response = await pokeApi.get(`/pokemon?limit=400&offset=${(page - 1) * 20}`);
+            const { data } = response;
+            const { results } = data;
+            const pokemonPromises = results.map(async (result) => {
+                const id = result.url.split('/').slice(-2, -1)[0];
+                const { data: pokemonData } = await pokeApi.get(`/pokemon/${id}`);
+                const { data: speciesData } = await pokeApi.get(`/pokemon-species/${pokemonData.id ?? pokemonData.name}`);
+                const evolutionChainUrl = speciesData?.evolution_chain?.url;
+                const evolutionChainId = evolutionChainUrl?.split('/').slice(-2, -1)[0];
+                const { data: evolutionChain } = await pokeApi.get(`/evolution-chain/${evolutionChainId}`);
+                const { chain } = evolutionChain;
+                const evolutionsList = [];
+                let currentChainItem = chain;
+                do {
+                    const { species, evolution_details: evolutionDetails } = currentChainItem;
+                    const id = species.url.split('/').slice(-2, -1)[0];
+                    const { data: speciesData } = await pokeApi.get(`/pokemon-species/${id}`);
+                    const {data} = await pokeApi.get(`/pokemon/${speciesData.id}`);
+                    const evolutions = new Evolution(
+                      species.name,
+                      data.sprites.other['official-artwork'].front_default,
+                      evolutionDetails[0],
+                    )
+                    evolutionsList.push(evolutions);
+                    currentChainItem = currentChainItem?.evolves_to?.[0];
+                } while (currentChainItem);
+                const abilities = pokemonData.abilities.map(({ ability }) => ability.name);
+                const moves = pokemonData.moves.map(({ move }) => move.name);
+                const stats = pokemonData.stats.map(({ base_stat, stat }) => ({ name: stat.name, value: base_stat }));
+                const types = pokemonData.types.map(({ type }) => type.name);
+                const genderRate = speciesData.gender_rate === -1 ? 'Genderless' : `${(100 - (speciesData.gender_rate * 10))}% Male, ${10 * speciesData.gender_rate}% Female`;
+                const generation = speciesData.generation.name;
+                const hatchSteps = 255 * ((speciesData.hatch_counter + 1) / 256);
+                const pokemon = new Pokemon(
+                  pokemonData.name,
+                  types,
+                  pokemonData.sprites.other['official-artwork'].front_default,
+                  pokemonData.id,
+                  evolutionsList,
+                  genderRate,
+                  hatchSteps,
+                  abilities,
+                  pokemonData.height,
+                  pokemonData.weight,
+                  moves,
+                  stats,
+                  generation,
+                );
+                return [id, pokemon];
+            });
+            const pokeList = await Promise.all(pokemonPromises);
+            const newPokeList = Object.fromEntries(pokeList);
+            setPokeList((prevPokeList) => ({ ...prevPokeList, ...newPokeList }));
+            setTotalPokePages(Math.ceil(data.count / 20));
+            setHasMorePoke(page < totalPokePages);
+        } catch (error) {
+            console.log(error);
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    }, [totalPokePages]);
 
     useEffect(() => {
-        const fetchPokemons = async () => {
-            try {
-                setLoading(true);
-                const response = await pokeApi.get('/pokemon?limit=900&offset=0');
-                const { data } = response;
-                const { results } = data;
-                const pokemonPromises = results.map(async (result) => {
-                    const id = result.url.split('/').slice(-2, -1)[0];
-                    const { data: pokemonData } = await pokeApi.get(`/pokemon/${id}`);
-                    const { data: speciesData } = await pokeApi.get(`/pokemon-species/${pokemonData.id ?? pokemonData.name}`);
-                    const evolutionChainUrl = speciesData?.evolution_chain?.url;
-                    const evolutionChainId = evolutionChainUrl?.split('/').slice(-2, -1)[0];
-                    const { data: evolutionChain } = await pokeApi.get(`/evolution-chain/${evolutionChainId}`);
-                    const { chain } = evolutionChain;
-                    const evolutionsList = [];
-                    let currentChainItem = chain;
-                    do {
-                        const { species, evolution_details: evolutionDetails } = currentChainItem;
-                        const id = species.url.split('/').slice(-2, -1)[0];
-                        const { data: speciesData } = await pokeApi.get(`/pokemon-species/${id}`);
-                        const {data} = await pokeApi.get(`/pokemon/${speciesData.id}`);
-                        const evolutions = new Evolution(
-                          species.name,
-                          data.sprites.other['official-artwork'].front_default,
-                          evolutionDetails[0],
-                        )
-                        evolutionsList.push(evolutions);
-                        currentChainItem = currentChainItem?.evolves_to?.[0];
-                    } while (currentChainItem);
-                    const abilities = pokemonData.abilities.map(({ ability }) => ability.name);
-                    const moves = pokemonData.moves.map(({ move }) => move.name);
-                    const stats = pokemonData.stats.map(({ base_stat, stat }) => ({ name: stat.name, value: base_stat }));
-                    const types = pokemonData.types.map(({ type }) => type.name);
-                    const genderRate = speciesData.gender_rate === -1 ? 'Genderless' : `${(100 - (speciesData.gender_rate * 10))}% ♂, ${10 * speciesData.gender_rate}% ♀`;
-                    const generation = speciesData.generation.name;
-                    const hatchSteps = 255 * ((speciesData.hatch_counter + 1) / 256);
-                    const pokemon = new Pokemon(
-                      pokemonData.name,
-                      types,
-                      pokemonData.sprites.other['official-artwork'].front_default,
-                      pokemonData.id,
-                      evolutionsList,
-                      genderRate,
-                      hatchSteps,
-                      abilities,
-                      pokemonData.height,
-                      pokemonData.weight,
-                      moves,
-                      stats,
-                      generation,
-                    );
-                    return pokemon;
-                });
-                const pokeList = await Promise.all(pokemonPromises);
-                setPokeList(pokeList);
-            } catch (error) {
-                console.log(error);
-                setError(true);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchPokemons();
-    }, []);
+        fetchPokemons(pokePage);
+    }, [fetchPokemons, pokePage]);
 
 
     useEffect(() => {
         localStorage.setItem('pokeList', JSON.stringify(pokeList));
     }, [pokeList]);
 
-    const getPokemonSpecies = async (name) => {
-        const { data } = await pokeApi.get(`/pokemon-species/${name}`);
-        return data;
-    }
 
-    const getPokemonInfo = async (name) => {
-        const { data } = await pokeApi.get(`/pokemon/${name}`);
-        return data;
-    }
-
-
-    const getPokemon = (name) => {
-        return pokeList.find((pokemon) => pokemon.name === name);
-    }
-
-    const getPokemonById = (id) => {
-        return pokeList.find((pokemon) => pokemon.id === id);
-    }
-
-    return { loading, error, pokeList, getPokemonSpecies, getPokemon, getPokemonById, getPokemonInfo };
+    return { loading, error, pokeList, hasMorePoke, setPokePage };
 };
