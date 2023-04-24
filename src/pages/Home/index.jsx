d2df792db5ed544345fe9b7aa627e89295f97530
buttowsk <vinicius_nunes0104@hotmail.com>
2023-04-24 01:33:38 -0300

    nome do usuario na home (não está implementado corretamente)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,18 +8,25 @@ import {
   Content,
 } from './styles';
 import { LoadingComponent } from '../../components/LoadingComponent/index.jsx';
+import {useEffect, useState} from "react";
 
 
 export const Home = ({ pokeList }) => {
+  const [name, setName] = useState('');
+  const nameFromLS = localStorage.getItem('name');
+  useEffect(() => {
+    setName(nameFromLS);
+  }, [nameFromLS]);
 
-  if (!pokeList) {
+  if (!pokeList || !name) {
     return <LoadingComponent/>;
   }
 
+  const username = name.charAt(0).toUpperCase() + name.slice(1);
   return (
     <Container>
       <Content>
-        <Title>O que está procurando?</Title>
+        <Title>Olá, { username }! O que está procurando?</Title>
         <List>
           <ListItem to={ '/pokedex' } buttonname={ 'pokedex' }>
             Pokemons
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -11,7 +11,6 @@ export const Container = styled.div`
   height: 100vh;
 `;
 
-
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
