9c9c9f729ecd8062a87c6c78aefedfd92a877bc3
buttowsk <vinicius_nunes0104@hotmail.com>
2023-04-24 01:31:12 -0300

    novos campos de cadastro

--- src/schemas/SignUp/index.js
+++ src/schemas/SignUp/index.js
@@ -1,7 +1,8 @@
 import * as yup from 'yup';
 
 export const SignUpSchema = yup.object().shape({
-  name: yup.string().required('Informe um nome'),
+  username: yup.string().required('Informe um nome de usuário'),
+  fullname: yup.string().required('Informe seu nome'),
   email: yup.string().email('Email inválido').required('Informe um email'),
   password: yup.string().required('Informe uma senha')
     .min(8, 'A senha deve ter no mínimo 8 caracteres')
