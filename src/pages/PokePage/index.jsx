255d809f3c54835ed66569b00e938f6865a75489
buttowsk <vinicius_nunes0104@hotmail.com>
2023-04-25 07:07:49 -0300

    verificando favoritos na pokepage

--- src/pages/PokePage/index.jsx
+++ src/pages/PokePage/index.jsx
@@ -10,7 +10,7 @@ import {
   BackButton,
   ElementsRow, TypeText, FavoriteButton,
 } from './styles.js';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { themes } from '../../globalStyles/styles.js';
 import { PokeAbout } from '../../components/PokeAbout/index.jsx';
@@ -19,16 +19,17 @@ import { PokeEvolutions } from '../../components/PokeEvolutions/index.jsx';
 import { useParams, useNavigate } from 'react-router-dom';
 import { LoadingComponent } from '../../components/LoadingComponent/index.jsx';
 import { useColors } from '../../hooks/useColors/index.js';
-import { dbApi } from '../../services/dbApi.js';
+import { FavoritesContext } from '../../contexts/favorites.jsx';
 
 export const PokePage = ({ pokeList, pokeSearch }) => {
+  const { favorites, addFavorite, removeFavorite, isFavorite } = useContext(FavoritesContext);
   const { pokemon } = useParams();
   const pokeArray = Object.values(pokeList);
   const navigate = useNavigate();
   const { useGetBgColor, getTextColor } = useColors();
   const [selectedPage, setSelectedPage] = useState('about');
   const [poke, setPoke] = useState({});
-  const [isFavorite, setIsFavorite] = useState(null);
+  const [favorite, setFavorite] = useState(null);
 
   useEffect(() => {
     async function getPokeData() {
@@ -54,62 +55,28 @@ export const PokePage = ({ pokeList, pokeSearch }) => {
     sessionStorage.setItem('poke', JSON.stringify(poke));
   }, [poke]);
 
+
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    const pokemon = JSON.parse(sessionStorage.getItem('poke'));
-    if (token) {
-      const checkFavorite = async () => {
-        const { data } = await dbApi.get('/get-user-id');
-        if (data && pokemon && pokemon.id) {
-          const response = await dbApi.get(`/users/${ data.user_id }/favorites/pokemon/${ pokemon.id }`);
-          if (response.status === 200 && response.data.favorite_pokemon.pokemon_name === pokemon.name) {
-            setIsFavorite('true');
-            console.log(response);
-          } else {
-            setIsFavorite('false');
-          }
-        }
-      };
-      checkFavorite();
+    if (isFavorite(poke)) {
+      setFavorite('true');
+    } else {
+      setFavorite('false');
     }
-  }, [pokemon, poke]);
-
-  const AddFavorite = async () => {
-    const { data } = await dbApi.get('/get-user-id');
-
-    if (data) {
-      const response = await dbApi.post(`/users/${ data.user_id }/favorites/pokemon`, {
-        'pokemon_name': `${ poke.name }`,
-        'pokemon_id': `${ poke.id }`,
-      });
-      if (response.status === 200) {
-        setIsFavorite('true');
-      }
-    }
-  };
-
-  const RemoveFavorite = async () => {
-    const { data } = await dbApi.get('/get-user-id');
-    const token = localStorage.getItem('token');
-
-    if (data) {
-      const response = await dbApi.delete(`/users/${ data.user_id }/favorites/pokemon/${ poke.id }`);
-      if (response.status === 200) {
-        setIsFavorite('false');
-      }
-    }
-  };
+  }, [favorites, poke]);
 
   const handleFavoriteClick = () => {
-    if (isFavorite === 'true') {
-      RemoveFavorite();
+    if (favorite === 'true') {
+      removeFavorite(poke);
     } else {
-      AddFavorite();
+      addFavorite(poke);
     }
   };
 
+  useEffect(() => {
+    console.log(favorites);
+  }, [favorites]);
 
-  if (!bgColor || !color || !poke || isFavorite === null) {
+  if (!bgColor || !color || !poke || favorite === null) {
     return <LoadingComponent/>;
   }
 
@@ -121,7 +88,7 @@ export const PokePage = ({ pokeList, pokeSearch }) => {
         <BackButton onClick={ () => navigate('/pokedex') }/>
         <FirstColumn>
           <IdText>#{ poke.id }</IdText>
-          <NameText>{ poke.name } <FavoriteButton onClick={ handleFavoriteClick } favorite={ isFavorite }/></NameText>
+          <NameText>{ poke.name } <FavoriteButton onClick={ handleFavoriteClick } favorite={ favorite }/></NameText>
           <ElementsRow>
             <TypeText theme={ themes[poke.types[0]] }>{ poke.types[0] }</TypeText>
             { poke.types[1] && <TypeText theme={ themes[poke.types[1]] }>{ poke.types[1] }</TypeText> }
